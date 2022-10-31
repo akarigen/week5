1afee8eab95889ac462bb9bb524030099224f459
Tabi Juzumambetova <tabihat.juzumambetova.ph@gmail.com>
2022-11-01 00:41:17 +0300

    main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+//1-задание
+let a = "10";
+let b = "20"
+
+let num1 = Number(a); 
+let num2 = Number(b);
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(num1+num2);
+//2-задание
+let  c = 12
+let d = 2
+
+let num3 = String(c);
+let num4 = String(d);
+console.log(num3+num4)
+//3-задание
+let num = 10;
+for (let i=0;i<=10;i++){
+    console.log(i)
+}
+//4-задание
+const second = 60
+const minute = 60
+const hour = 24
+let days = 365
+console.log(`в году ${second*minute*hour*days} секунд`)
